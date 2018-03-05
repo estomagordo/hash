2b5ee89508ed1b61481134f82ce172c49157ccb4
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-03-05 18:20:10 +0100

    runner

---
+++ 2018/qual/runner.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == '__main__':
+    os.system('python drive.py a_example')
+    os.system('python drive.py b_should_be_easy')
+    os.system('python drive.py c_no_hurry')
+    os.system('python drive.py d_metropolis')
+    os.system('python drive.py e_high_bonus')
