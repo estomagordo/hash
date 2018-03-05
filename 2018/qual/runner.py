33491d4f140ea3e82334d772aa6011134fdb7200
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-03-05 18:41:33 +0100

    color shenanigans

--- 2018/qual/runner.py
+++ 2018/qual/runner.py
@@ -2,7 +2,12 @@ import os
 
 if __name__ == '__main__':
     os.system('python drive.py a_example')
+    os.system('color 01')
     os.system('python drive.py b_should_be_easy')
+    os.system('color 02')
     os.system('python drive.py c_no_hurry')
+    os.system('color 04')
     os.system('python drive.py d_metropolis')
+    os.system('color 06')
     os.system('python drive.py e_high_bonus')
+    os.system('color 0f')
